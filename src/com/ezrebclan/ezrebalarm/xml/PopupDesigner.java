97972a5add7e38c9765ba072af94ce3f0dd03da1
MrabEzreb <de013b3f@opayq.com>
2015-04-23 14:33:51 -0400

    heading home :D

--- src/com/ezrebclan/ezrebalarm/xml/PopupDesigner.java
+++ src/com/ezrebclan/ezrebalarm/xml/PopupDesigner.java
@@ -9,10 +9,19 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Window.Type;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class PopupDesigner extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtTitle;
+	private JTextField txtMessage;
+	private JTextField txtSecondMessage;
+	private JTextField txtCloseButtonText;
+	private JButton btnSave;
+	private JTextField txtFileName;
 
 	/**
 	 * Launch the application.
@@ -40,7 +49,36 @@ public class PopupDesigner extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 300, 50));
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 25));
+		
+		txtTitle = new JTextField();
+		txtTitle.setText("Title");
+		contentPane.add(txtTitle);
+		txtTitle.setColumns(25);
+		
+		txtMessage = new JTextField();
+		txtMessage.setText("Message");
+		contentPane.add(txtMessage);
+		txtMessage.setColumns(25);
+		
+		txtSecondMessage = new JTextField();
+		txtSecondMessage.setText("Second Message");
+		contentPane.add(txtSecondMessage);
+		txtSecondMessage.setColumns(25);
+		
+		txtCloseButtonText = new JTextField();
+		txtCloseButtonText.setText("Close Button Text");
+		contentPane.add(txtCloseButtonText);
+		txtCloseButtonText.setColumns(25);
+		
+		txtFileName = new JTextField();
+		txtFileName.setHorizontalAlignment(SwingConstants.CENTER);
+		txtFileName.setText("File Name");
+		contentPane.add(txtFileName);
+		txtFileName.setColumns(10);
+		
+		btnSave = new JButton("Save");
+		contentPane.add(btnSave);
 	}
 
 }
